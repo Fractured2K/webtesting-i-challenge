165dcf39f2484493a8984a665b8aede5d9f7df51
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-08 11:47:53 -0700

    Create repair test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,3 +1,13 @@
 const { succeed, fail, repair, get } = require('./enhancer.js');
 
-describe('Enhancer tests', () => {});
+describe('Enhancer tests', () => {
+	test('accepts item (obect) and returns new item (object) with the durability restored to 100', () => {
+		expect(
+			repair({ name: 'Staff', enhancement: 6, durabilty: 20 })
+		).toEqual({
+			name: 'Staff',
+			enhancement: 6,
+			durabilty: 100
+		});
+	});
+});
