700b49f2b4a4701959457817da74a62872062a56
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-08 13:10:44 -0700

    Refactor enhancer tests file structure

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,8 +1,7 @@
 const { succeed, fail, repair, get } = require('./enhancer.js');
 
-describe('Enhancer tests', () => {
-	// Enhancer succeed method tests
-	test('accepts item (object) and returns new item (object). if enhancement level is less than 20 go up by one if not stay the same', () => {
+describe('Enhancer succeed method tests', () => {
+	test('If the items enhancement level is less than 20 go up by one if not stay the same', () => {
 		// check to see if item enhancement level goes up by 1
 		expect(
 			succeed({ name: 'Staff', enhancement: 6, durabilty: 20 })
@@ -21,10 +20,11 @@ describe('Enhancer tests', () => {
 			durabilty: 20
 		});
 	});
+});
 
-	// Enhancer fail method tests
-	test('accepts item (obect) and returns new item (object) based on rules defined by the client.', () => {
-		// If the item's enhancement is less than 15, the durability of the item is decreased by 5
+describe('Enhancer fail method tests', () => {
+	test('If the items enhancement level is less than 15, the durability of the item is decreased by 5.', () => {
+		// If the items enhancement is less than 15, the durability of the item is decreased by 5
 		expect(fail({ name: 'Staff', enhancement: 14, durabilty: 20 })).toEqual(
 			{
 				name: 'Staff',
@@ -32,8 +32,10 @@ describe('Enhancer tests', () => {
 				durabilty: 15
 			}
 		);
+	});
 
-		// If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+	test('If the items enhancement level is 15 or more, the durability of the item is decreased by 10.', () => {
+		// If the items enhancement is 15 or more, the durability of the item is decreased by 10.
 		expect(fail({ name: 'Staff', enhancement: 15, durabilty: 20 })).toEqual(
 			{
 				name: 'Staff',
@@ -41,8 +43,10 @@ describe('Enhancer tests', () => {
 				durabilty: 10
 			}
 		);
+	});
 
-		// If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
+	test('If the items enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).', () => {
+		// If the items enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
 		expect(fail({ name: 'Staff', enhancement: 18, durabilty: 20 })).toEqual(
 			{
 				name: 'Staff',
@@ -51,9 +55,10 @@ describe('Enhancer tests', () => {
 			}
 		);
 	});
+});
 
-	// Enhancer repair method tests
-	test('accepts item (obect) and returns new item (object) with the durability restored to 100', () => {
+describe('Enhancer repair method tests', () => {
+	test('Accepts item (obect) and returns new item (object) with the durability restored to 100', () => {
 		expect(
 			repair({ name: 'Staff', enhancement: 6, durabilty: 20 })
 		).toEqual({
