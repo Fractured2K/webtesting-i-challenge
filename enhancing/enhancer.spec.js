b61be9af59fbc73b7d5dab0ef886a8f1c0eb48db
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-08 12:24:32 -0700

    Create fail test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,7 +1,7 @@
 const { succeed, fail, repair, get } = require('./enhancer.js');
 
 describe('Enhancer tests', () => {
-	// Succeed
+	// Enhancer succeed method tests
 	test('accepts item (object) and returns new item (object). if enhancement level is less than 20 go up by one if not stay the same', () => {
 		// check to see if item enhancement level goes up by 1
 		expect(
@@ -22,7 +22,19 @@ describe('Enhancer tests', () => {
 		});
 	});
 
-	// Repair
+	// Enhancer fail method tests
+	test('accepts item (obect) and returns new item (object) based on rules defined by the client.', () => {
+		// If the item's enhancement is less than 15, the durability of the item is decreased by 5
+		expect(
+			succeed({ name: 'Staff', enhancement: 14, durabilty: 20 })
+		).toEqual({
+			name: 'Staff',
+			enhancement: 14,
+			durabilty: 15
+		});
+	});
+
+	// Enhancer repair method tests
 	test('accepts item (obect) and returns new item (object) with the durability restored to 100', () => {
 		expect(
 			repair({ name: 'Staff', enhancement: 6, durabilty: 20 })
