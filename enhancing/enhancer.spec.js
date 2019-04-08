6e41880972bc58e356c0086dc64f3babe33d4c77
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-08 12:13:47 -0700

    Create second succeed test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -3,11 +3,21 @@ const { succeed, fail, repair, get } = require('./enhancer.js');
 describe('Enhancer tests', () => {
 	// Succeed
 	test('accepts item (object) and returns new item (object) modified defined by the client for enhancement success', () => {
+		// check to see if item enhancement level goes up by 1
 		expect(
 			succeed({ name: 'Staff', enhancement: 6, durabilty: 20 })
 		).toEqual({
 			name: 'Staff',
 			enhancement: 7,
+			durabilty: 20
+		});
+
+		// check to see if item enhancement level stays at 20
+		expect(
+			succeed({ name: 'Staff', enhancement: 20, durabilty: 20 })
+		).toEqual({
+			name: 'Staff',
+			enhancement: 20,
 			durabilty: 20
 		});
 	});
